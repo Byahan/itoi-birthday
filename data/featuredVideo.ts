17cdc007a7aa50028922b7531852241b9764406b
Byahan <codenameinv@gmail.com>
2026-08-14 05:10:59 +0700

    shorts

--- data/featuredVideo.ts
+++ data/featuredVideo.ts
@@ -1,6 +1,6 @@
 export const featuredVideo = {
-  title: "同期に悪戯をしまくってニコニコな絲依とい #GooNECT√2 #vtuber #絲依とい #shorts",
+  title: "反射が怖くて紹介できな絲依とい #shorts #絲依とい #vtuber",
   description: "",
-  videoId: "65OAZC3t5YA",
+  videoId: "M4EwKm9_2RU",
   thumbnail: "/images/featured-video.jpg",
 };
